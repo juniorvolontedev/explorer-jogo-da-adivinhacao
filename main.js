1bacff148eebc660540d93977d164277356d6dfa
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-02 04:19:09 -0300

    refatorando e algumas funcionalidades a mais

--- main.js
+++ main.js
@@ -1,36 +1,50 @@
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
 
-const randomNumber = Math.round(Math.random() * 10);
+let randomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
+
+btnTry.addEventListener("click", handleTryClick);
+
+btnReset.addEventListener("click", handleResetClick);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Enter" && screen1.classList.contains("hide")) {
+    handleResetClick();
+  }
+});
 
 function handleTryClick(event) {
   event.preventDefault();
 
   const inputNumber = document.querySelector("#inputNumber");
 
-  if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide");
-    screen2.classList.remove("hide");
+  if (inputNumber.value >= 0 && inputNumber.value <= 10) {
+    if (Number(inputNumber.value) == randomNumber) {
+      toggleScreen();
 
-    screen2.querySelector(
-      "h2"
-    ).innerText = `Acertou em ${xAttempts} tentativas!`;
+      screen2.querySelector(
+        "h2"
+      ).innerText = `Acertou em ${xAttempts} tentativas!`;
+    }
+
+    xAttempts++;
+  } else {
+    alert("O número precisa ser entre 0 e 10");
   }
 
   inputNumber.value = "";
-
-  xAttempts++;
 }
 
-const btnTry = document.querySelector("#btnTry");
-const btnReset = document.querySelector("#btnReset");
-
-btnTry.addEventListener("click", handleTryClick);
-
-btnReset.addEventListener("click", () => {
-  screen1.classList.remove("hide");
-  screen2.classList.add("hide");
-
+function handleResetClick() {
+  toggleScreen();
   xAttempts = 1;
-});
+  randomNumber = Math.round(Math.random() * 10);
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
